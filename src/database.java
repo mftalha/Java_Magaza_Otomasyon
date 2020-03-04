7d311a916128ddc587428d08ec25beeef78999e3
Talha <mtalha0729@hotmail.com>
2020-03-04 14:24:53 +0300

    Java_Magaza_Otomasyon

---
+++ src/database.java
@@ -0,0 +1,14 @@
+public class database 
+{
+    public static final String kullanici_adi ="root";
+    
+    public static final String parola ="";
+    
+    public static final String database_adı ="magza_2";
+    
+    public static final String port ="3306";
+    
+    public static final String host ="localhost";
+    
+    
+}
